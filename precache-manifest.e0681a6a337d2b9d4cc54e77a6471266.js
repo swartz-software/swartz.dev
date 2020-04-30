0eb1b3f275872563b48318bf0c38c82626f16e29
Ian MacKay <immackay0@gmail.com>
2020-04-29 21:37:43 -0700

    Updates

---
+++ precache-manifest.e0681a6a337d2b9d4cc54e77a6471266.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "290e1f129c2086a924e8a603dbc25932",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9745ae120790e52c2fb7",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "a8a09ad15eb063a387db",
+    "url": "/static/css/main.745f5315.chunk.css"
+  },
+  {
+    "revision": "9745ae120790e52c2fb7",
+    "url": "/static/js/2.76265a4b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.76265a4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8a09ad15eb063a387db",
+    "url": "/static/js/main.e5060fe8.chunk.js"
+  },
+  {
+    "revision": "c0b9ed5563de64be13d5",
+    "url": "/static/js/runtime-main.92fd5fd7.js"
+  }
+]);
